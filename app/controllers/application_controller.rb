c3140e0c25289d047ed5ab33d1a07008d8259604
Amil Masic <amil.masic@gmail.com>
2019-07-27 13:47:50 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 require_relative '../../config/environment'
 class ApplicationController < Sinatra::Base
+  register Sinatra::ActiveRecordExtension
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
